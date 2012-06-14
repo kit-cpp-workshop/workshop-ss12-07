6a058a216b1b9e9b90e680ba7bd29c5c51f9abe5
Robert Schneider <Robert.Schneider3@student.kit.edu>
2012-06-15 00:31:01 +0200

    Adds example for observable behavior

---
+++ slides/cpp-code/observable-behaviour.cpp
@@ -0,0 +1,28 @@
+/*$ begin $*/
+void unobservable(int& p)
+{
+	int undetermined = 40;
+	undetermined += 2;
+	
+	p = i;
+}
+
+/*$ observable $*/
+void observable()
+{
+	volatile int visible = 21;
+	visible *= 2;
+/*$ invisible $*/
+	int invisible = 44;
+	invisible -= 2;
+/*$ not_yet_visible $*/
+	int not_yet_vis = 84;
+	not_yet_vis /= 2;
+/*$ function_calls $*/
+	observable(visible);
+	unobservable(invisible);
+	unobservable(not_yet_vis);
+/*$ IO $*/
+	std::cout << visible;
+	std::cout << not_yet_vis;	// now visible!
+}
