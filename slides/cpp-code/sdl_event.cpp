8990991c0e428d64369f004ce109c5441da1c9fa
dfyx <dfyx@wecallit42.de>
2012-06-15 16:27:56 +0200

    Add SDL slides

---
+++ slides/cpp-code/sdl_event.cpp
@@ -0,0 +1,17 @@
+while(1){
+    SDL_Event event;
+    while(SDL_PollEvent(&event)) {  /* Loop until there are no events left on the queue */
+	  switch(event.type) { /* Process the appropriate event type */
+	    case SDL_KEYDOWN:  /* Handle a KEYDOWN event */
+	      printf("Oh! Key press\n");
+	      break;
+	    case SDL_MOUSEMOTION:
+	      .
+	      .
+	      .
+	    default: /* Report an unhandled event */
+	      printf("I don't know what this event is!\n");
+	  }
+	}
+    // do some other stuff here -- draw your app, play your music, perform other operations...
+}
